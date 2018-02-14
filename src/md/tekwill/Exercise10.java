b2f7208ab87b42d741323b901f515334a7295817
dboldureanu <dboldureanu@gmail.com>
2018-02-14 17:05:47 +0200

    - bug fix

--- src/md/tekwill/Exercise10.java
+++ src/md/tekwill/Exercise10.java
@@ -6,7 +6,7 @@ public class Exercise10 {
     Given an array of strings and a letter,
     display the elements that contain the letter.
     IN: Array: {"One", "Two", "Three"}, Letter: e
-    OUT: One, Two
+    OUT: One, Three
     */
     public static void main(String[] args) {
         // write your code here
--- src/md/tekwill/Exercise8.java
+++ src/md/tekwill/Exercise8.java
@@ -3,9 +3,9 @@ package md.tekwill;
 public class Exercise8 {
 
     /*
-    Given an array - determine its length.
+    Given an string array - determine the sum of the letters of the elements.
     IN: {"One", "Two", "Three"}
-    OUT: 3
+    OUT: 11
     */
     public static void main(String[] args) {
         // write your code here
