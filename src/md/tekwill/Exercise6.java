fa4c3845c79164e8e1d4393702c4a30beb9ec634
Olecika Darii <olealea001@gmail.com>
2018-02-16 20:47:50 +0200

    homework 5

--- src/md/tekwill/Exercise6.java
+++ src/md/tekwill/Exercise6.java
@@ -9,17 +9,6 @@ public class Exercise6 {
     {"Java", "Developer", "Software"}
     OUT: Developer
     */
-    public static String getLongestString(String[] array) {
-        int maxLength = 0;
-        String longestString = null;
-        for (String s : array) {
-            if (s.length() > maxLength) {
-                maxLength = s.length();
-                longestString = s;
-            }
-        }
-        return longestString;
-    }
 
     public static void main(String[] args) {
 
