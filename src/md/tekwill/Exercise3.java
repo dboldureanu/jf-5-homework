7925d157683258c5339da29401c83f446d843625
shyene27 <32588214+shyene27@users.noreply.github.com>
2018-02-23 10:29:11 +0200

    Update Exercise3.java

--- src/md/tekwill/Exercise3.java
+++ src/md/tekwill/Exercise3.java
@@ -1,13 +1,30 @@
 package md.tekwill;
 
+
+
 public class Exercise3 {
 
+
+
     /*
+
     Write a program that would determine minimal number in an array.
+
     Input: {256, 16, 32, 8, 4, 512}
+
     OUT: 4
+
     */
+
     public static void main(String[] args) {
-        // write your code here
+
+     int numbers[] = {256, 16, 32, 8, 4, 512};
+     int  min=numbers[0];
+     for(int i=0; i<numbers.length; i++){
+        
+         if (min>numbers[i]) min = numbers[i];
+     }
+    System.out.println("The minimal number is " + min);
     }
+
 }
