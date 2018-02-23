b7dda0dccebc6fa7c6656fb6c0295595261a8b4e
shyene27 <32588214+shyene27@users.noreply.github.com>
2018-02-23 10:56:37 +0200

    Update Exercise5.java

--- src/md/tekwill/Exercise5.java
+++ src/md/tekwill/Exercise5.java
@@ -1,13 +1,27 @@
 package md.tekwill;
 
+
+
 public class Exercise5 {
 
+
+
     /*
+
     Given a string, determine its length.
+
     IN: "Java"
+
     OUT: 4
+
     */
+
     public static void main(String[] args) {
-        // write your code here
+
+    String text = new String ("Java");
+
+    System.out.println(text.length());
+
     }
+
 }
